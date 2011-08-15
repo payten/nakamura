a9b51db9dfed66188a160048f304a31eb7c12357
Erik Froese <erik.froese@gmail.com>
2011-08-15 15:59:25 -0400

    Get rid of the eclipse warning.

--- contrib/grouper/src/main/java/org/sakaiproject/nakamura/grouper/exception/InvalidGroupIdException.java
+++ contrib/grouper/src/main/java/org/sakaiproject/nakamura/grouper/exception/InvalidGroupIdException.java
@@ -1,6 +1,7 @@
 package org.sakaiproject.nakamura.grouper.exception;
 
 public class InvalidGroupIdException extends GrouperException {
+	private static final long serialVersionUID = 1L;
 
 	public InvalidGroupIdException(String message) {
 		super(message);
