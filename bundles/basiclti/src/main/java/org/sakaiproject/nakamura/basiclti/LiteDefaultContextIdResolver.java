81a455da02ead0fe21854abe5ff23ac3b5603b79
Lance Speelmon <lance@speelmon.com>
2012-05-24 09:26:04 -0700

    KERN-2860 Privilege escalation in LiteDefaultContextIdResolver (cherry picked from commit ff2aaf7e37f6eabf3edc31af350dbd0a41b8db4e)
    Conflicts:
    	bundles/basiclti/src/main/java/org/sakaiproject/nakamura/basiclti/LiteDefaultContextIdResolver.java

--- bundles/basiclti/src/main/java/org/sakaiproject/nakamura/basiclti/LiteDefaultContextIdResolver.java
+++ bundles/basiclti/src/main/java/org/sakaiproject/nakamura/basiclti/LiteDefaultContextIdResolver.java
@@ -17,22 +17,21 @@
  */
 package org.sakaiproject.nakamura.basiclti;
 
-import static org.sakaiproject.nakamura.api.basiclti.BasicLTIAppConstants.LTI_VTOOL_ID;
-
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Property;
-import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.commons.osgi.PropertiesUtil;
 import org.osgi.service.component.ComponentContext;
 import org.sakaiproject.nakamura.api.basiclti.LiteBasicLTIContextIdResolver;
-import org.sakaiproject.nakamura.api.basiclti.VirtualToolDataProvider;
 import org.sakaiproject.nakamura.api.lite.Session;
 import org.sakaiproject.nakamura.api.lite.StorageClientException;
 import org.sakaiproject.nakamura.api.lite.accesscontrol.AccessDeniedException;
 import org.sakaiproject.nakamura.api.lite.authorizable.AuthorizableManager;
 import org.sakaiproject.nakamura.api.lite.authorizable.Group;
 import org.sakaiproject.nakamura.api.lite.content.Content;
+import org.sakaiproject.nakamura.api.lite.Session;
+import org.sakaiproject.nakamura.api.lite.StorageClientException;
+import org.sakaiproject.nakamura.api.lite.accesscontrol.AccessDeniedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -52,25 +51,21 @@ public class LiteDefaultContextIdResolver implements LiteBasicLTIContextIdResolv
   private static final Logger LOG = LoggerFactory
       .getLogger(LiteDefaultContextIdResolver.class);
 
-  //TODO eventually needs to be a list of providers not just a single provider.
-  @Reference
-  protected transient VirtualToolDataProvider virtualToolDataProvider;
-  
   /**
    * The {@link Content} property key used to store an LTI context_id.
    */
   @Property(value = "lti_context_id", name = "LTI_CONTEXT_ID", label = "key", description = "The Content property key used to store an LTI context_id.")
   public static final String LTI_CONTEXT_ID = LiteDefaultContextIdResolver.class
       .getName() + ".key";
-  private String key = "lti_context_id";
+  protected String key = "lti_context_id";
 
   /**
    * {@inheritDoc}
    * 
    * @see {@link LiteBasicLTIContextIdResolver#resolveContextId(Content)}
    */
-  public String resolveContextId(final Content node, String groupId, Session session)
-      throws AccessDeniedException, StorageClientException {
+  public String resolveContextId(final Content node, final String groupId,
+      final Session session) throws AccessDeniedException, StorageClientException {
     LOG.debug("resolveContextId(Content {}, String {}, Session session)", node, groupId);
     if (node == null) {
       throw new IllegalArgumentException("Content cannot be null");
@@ -78,49 +73,61 @@ public class LiteDefaultContextIdResolver implements LiteBasicLTIContextIdResolv
     if (session == null) {
       throw new IllegalArgumentException("Session cannot be null");
     }
-    
+
     final AuthorizableManager authManager = session.getAuthorizableManager();
 
-    String contextId = null;
-    if (groupId != null) {
-      contextId = groupId;
-      Group group = (Group) authManager.findAuthorizable(groupId);
-      LOG.debug("group = {}", group);
-      // If we are using a Group, we check the group's node to see if it has the optional
-      // cle-site property which will correspond to a concrete site on the CLE installs that
-      // we wish to use for the context on Sakai2Tools widgets. 
-      // After that we also check to see if the current vtoolid is actually a Sakai2Tool
-      // ( it could be from a completely seperate location, etherpad etc. ). If it is
-      // a Sakai 2 Tool, then we use the cle-site property as the context for that BLTI
-      // launch.
-      if (group != null) {
-        final String sakaiSite = (String) group.getProperty("sakai:cle-site");
-        if (sakaiSite != null) {
-          // Check and see if this is a Sakai Site
-          // Right now the only virtualtoolprovider is for Sakai CLE.
-          final String vtoolid = (String) node.getProperty(LTI_VTOOL_ID);
-          if (vtoolid != null &&
-              virtualToolDataProvider.getSupportedVirtualToolIds().contains(vtoolid)) {
-            contextId = sakaiSite;
+    // default return value
+    String contextId = node.getPath();
+
+    // check the alternate key for a context ID
+    if (node.hasProperty(key)) {
+      // we have a special context_id we can use
+      contextId = (String) node.getProperty(key);
+    } else {
+      if (groupId != null) {
+        // obtaining the group causes a security check; also the group is used later to
+        // check the sakai:cle-site prop.
+        final Group group = (Group) authManager.findAuthorizable(groupId);
+        LOG.debug("group = {}", group);
+
+        /*
+         * If we are using a Group, we check the group's node to see if it has one of the
+         * optional properties that control LTI_CONTEXT_ID: "lti_context_id" or
+         * "sakai:cle-site". The property "lti_context_id" will take precedence over
+         * "sakai:cle-site". They are used to manually control LTI mapping to particular
+         * contexts and are passed straight through verbatim.
+         */
+        if (group != null) {
+          // by default, just use the groupID that was submitted
+          contextId = groupId;
+
+          // LTI_CONTEXT_ID takes precedence over "sakai:cle-site" property
+          if (group.hasProperty(key)) {
+            // we have a special context_id we can use
+            final String groupContextId = (String) group.getProperty(key);
+            if (groupContextId != null && !"".equals(groupContextId)) {
+              contextId = groupContextId;
+            }
+          } else {
+            // try "sakai:cle-site" property next
+            final String sakaiSite = (String) group.getProperty("sakai:cle-site");
+            if (sakaiSite != null && !"".equals(sakaiSite)) {
+              contextId = sakaiSite;
+            }
           }
         }
       }
     }
-    else {
-      if (node.hasProperty(key)) {
-        // we have a special context_id we can use
-        contextId = (String) node.getProperty(key);
-      } else {
-        // just use the path
-        contextId = node.getPath();
-      }
-    }
+    LOG.debug("Using context id '{}' for node: {}", contextId, node.getPath());
     return contextId;
   }
 
   protected void activate(ComponentContext context) {
     @SuppressWarnings("rawtypes")
     final Dictionary props = context.getProperties();
-    key = PropertiesUtil.toString(props.get(LTI_CONTEXT_ID), "lti_context_id");
+    final String newKey = PropertiesUtil.toString(props.get(LTI_CONTEXT_ID), key);
+    if (!"".equals(newKey)) {
+      key = newKey;
+    }
   }
 }
